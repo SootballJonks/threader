d1675e84e595e419920b1aeb60a14aa1146e34e5
Jonks <anastasiablackhold@gmail.com>
2021-10-05 18:48:20 -0700

    Event handler added

--- threader.js
+++ threader.js
@@ -1,4 +1,4 @@
-//imports
+//------------IMPORTS-------------------
 const fs = require("fs");
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require("./config.json");
@@ -6,7 +6,25 @@ const { token } = require("./config.json");
 //Client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+//-------------EVENT LOGIC--------------
+
+const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
+
+
+//--------SLASH COMMANDS LOGIC----------
+
 client.commands = new Collection();
+
 const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 
 for (const file of commandFiles) {
@@ -14,31 +32,8 @@ for (const file of commandFiles) {
   client.commands.set(command.data.name, command);
 }
 
-client.once("ready", () => {
-  console.log("Ready!");
-});
+//--------------------------------------
 
-client.on("interactionCreate", async (interaction) => {
-  if(!interaction.isCommand()) {
-    return;
-  };
-
-  const command = client.commands.get(interaction.commandName);
-
-  if (!command) {
-    return;
-  };
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    return interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true
-    })
-  };
-});
 
 
 client.login(token);
